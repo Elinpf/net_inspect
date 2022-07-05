5ea328408f4b08aa0683a976ebadc39af3f09a7f
Elin <365433079@qq.com>
2022-07-05 17:11:12 +0800

    ✨ feat: update version to v0.2.0

--- tests/test_net_inspect.py
+++ tests/test_net_inspect.py
@@ -1,5 +1,12 @@
 from net_inspect import __version__
+import os.path as p
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    ver = '0.2.0'
+
+    assert __version__ == ver
+    setup_file = p.join(p.dirname(p.dirname(__file__)), 'pyproject.toml')
+    with open(setup_file, 'r', encoding='utf8') as f:
+        content = f.read()
+        assert f'version = "{ver}"' in content
