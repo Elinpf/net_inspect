5eb8eab656be6a4af9272fe93c95ef1da32c7758
Elin <365433079@qq.com>
2022-09-16 15:10:04 +0800

    ✨ feat: update version to v1.3.5

--- tests/test_net_inspect.py
+++ tests/test_net_inspect.py
@@ -3,7 +3,7 @@ import os.path as p
 
 
 def test_version():
-    ver = '1.3.4'
+    ver = '1.3.5'
 
     assert __version__ == ver
     setup_file = p.join(p.dirname(p.dirname(__file__)), 'pyproject.toml')
